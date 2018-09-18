54272bba392cbe166bdbac6b74156878cdf97304
Ben Dixon <bendixon311@gmail.com>
2018-09-17 21:33:18 -0600

    Email not working yet

---
+++ Unit01/receive.php
@@ -0,0 +1,44 @@
+<?php
+$gender = $_POST[gender];
+$name = $_POST[name];
+$email = $_POST[emailaddress];
+$phone= $_POST[phone];
+$comments = $_POST[input];
+
+
+
+//BUILD EMAIL
+$to = "bendixon311@gmail.com";
+$subject = "Form from Unit 01";
+$message = "$name has sent you some comments.
+INFO:
+Gender: $gender
+
+Email: $email
+
+Phone: $phone
+
+Comments: $input
+
+";
+
+//SEND EMAIL
+
+mail($to, $subject, $message, "FROM: ".$email);
+
+
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+		<title>Submission confirmed!</title>
+	</head>
+	
+	<body>
+	<p>Thank you, <?php echo $name; ?>, for your comments.</p>
+	</body>
+</html>
