a59c489f1d128c7be730986feba86ebc29ef6e06
Ben Dixon <bendixon311@gmail.com>
2018-09-18 13:54:54 -0600

    final

--- Unit01/receive_wmsb6f9nz2.php
+++ Unit01/receive_wmsb6f9nz2.php
@@ -67,3 +67,7 @@ echo $message;
 		</div>
 	</body>
 </html>
+
+<?php
+echo "<mm:dwdrfml documentRoot=" . __FILE__ .">";$included_files = get_included_files();foreach ($included_files as $filename) { echo "<mm:IncludeFile path=" . $filename . " />"; } echo "</mm:dwdrfml>";
+?>
