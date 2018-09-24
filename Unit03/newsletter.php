a1b43c7d3a848c2bf305b1240793226296bf2f6b
Ben Dixon <bendixon311@gmail.com>
2018-09-24 14:31:51 -0600

    form is working

--- Unit03/newsletter.php
+++ Unit03/newsletter.php
@@ -1,15 +1,15 @@
 <?php
-$gender = $_POST['gender'];
+
 $name = $_POST['name'];
 $email = $_POST['emailaddress'];
 $phone= $_POST['phone'];
-$color = $_POST['favcolor'];
+
 
 //establish DB connection
-$dbconnect = mysqli_connect('localhost','bendix7_dgm3760','epitaph311','bendix7_dgm3760') or die('unable to connect to db');
+$dbconnect = mysqli_connect('localhost','bendix7_dgm3760','epitaph311','bendix7_dgm3760') or die('unable to connect to database');
 
 //build sql query
-$myQuery= "INSERT INTO `test_inquiries`(`gender`, `name`, `email`, `phone`, `color`) VALUES ('$gender','$name','$email','$phone','$color')";
+$myQuery= "INSERT INTO newsletter(name, email, phone) VALUES ('$name','$email','$phone')";
 
 
 //talk to DB
@@ -32,7 +32,7 @@ mysqli_close($dbconnect);
 	<body>
 		<div class="container">
 			<form id="contact">
-	<h3><?php echo $name; ?>, you've been added to the database</h3>
+	<h3><?php echo $name; ?>, Thanks for signing up for the newsletter!</h3>
 				</form>
 		</div>
 	</body>
