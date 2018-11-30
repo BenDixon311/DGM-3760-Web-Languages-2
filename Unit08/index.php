1b1174a08f508d7e02496e0a86fe94afe0369c74
Ben Dixon <bendixon311@gmail.com>
2018-11-29 21:45:50 -0700

    finished unit 8

--- Unit08/index.php
+++ Unit08/index.php
@@ -1,118 +1,79 @@
 <?php
+
+
+$searchid = '';
+$team = $_GET[team_id];
+
+
+if (isset($_GET[team_id])){
+	
+	$searchid = "WHERE west_players.team_id=$team";
+
+}
+
 require_once('variables.php');
 
+$dbconnect = mysqli_connect(HOST, USER, PASSWORD, DB_NAME) or die("unable to connect to db");
+
+//build query for INNER JOIN
+$query = "SELECT * FROM west_players INNER JOIN west_teams ON (west_players.team_id = west_teams.team_id) $searchid ORDER BY last";
 
 
-	$dbconnect = mysqli_connect(HOST, USER, PASSWORD, DB_NAME) or die("unable to connect to db");
 
-//get team names
-$query = "SELECT * FROM west_teams";
-$resultTeams = mysqli_query($dbconnect, $query) or die('failed to query teams');
-
-//get position names
-$query = "SELECT * FROM west_positions ORDER BY position_name ASC";
-$resultPositions = mysqli_query($dbconnect, $query) or die ('failed to query positions');
-
+$result = mysqli_query($dbconnect, $query) or die ('inner join query did not work');
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+
+<!doctype html>
+<html>
 <head>
-	<meta charset="utf-8">
-	<title>Sign Up</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<meta charset="utf-8">
+<title>NBA Players</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
 </head>
 
-<body id="home">
+<body>
 	
-	<!--navigation-->
-
-	<?php
-		include_once('navbar.php');
+	<?php include_once('navbar.php');  ?>
 	
-	
-	
+	<div class="tablecontainer">
+		<form class="contact">
 		
-	?>
-	
-	<div class="container">
-	
-	
-	
-	<form class="contact" action="saveToDB.php" method="POST" enctype="multipart/form-data">
-	
-	
-	
-	
-	
-<div class="container">
-	
-	
-	
-	<form class="contact" action="add.php" method="POST" enctype="multipart/form-data">
-		
-		<h2>Add Player</h2>
-   
-
-    <fieldset>
-		<legend>NAME</legend>
-		First Name: <input type = "text" name = "first" required><br>
-		Last Name: <input type = "text" name = "last"  required><br>
-		
-		</fieldset>
-		<fieldset>
-			<legend>DETAILS</legend>
-       
-        
-			Select Team:
-			<select name = "team">
-			<option>Please select...</option>
-			
-			
 			<?php
-			while($row = mysqli_fetch_array($resultTeams)) {
-				echo '<option value= "'.$row['team_id'].'">'.$row['team_name'].'</option>';
-			}
+				while ($row = mysqli_fetch_array($result)) {
+					//TERNARY OPERATOR
+					echo '<div class = "playercard">';
+					
+					echo '<p class="playername">'.$row['first'].' '.$row['last'];
+					echo ($row['team_id'] == 2 ? ' - 2018 NBA Champion</p> ' : '</p>');
+					
+					echo $row['team_name'].'</div>';
+					
+					echo '<div class = "playerdetails"><p>'.$row['first'].' plays the following positions: </p>';
+					
+					$playerid = $row['id'];
+					
+					$query2 = "SELECT * FROM west_skills INNER JOIN west_positions ON (west_skills.position_id = west_positions.position_id) WHERE id=$playerid";
+					
+					$resultPos = mysqli_query($dbconnect, $query2) or die ('unable to query positions');
+					
+					while ($posrow = mysqli_fetch_array($resultPos)) {
+						echo '<p class="positions">'.$posrow['position_name'].'</p>';
+					}
+					
+					echo '</div>';
+					
+					
+					
+				}
 			
 			?>
-			</select><br>
-			
-			Select Positions<br>
-			<?php
-			while($row = mysqli_fetch_array($resultPositions)) {
-				echo '<label><input type = "checkbox" value = "'.$row['position_id'].'" name = "positions[]">'.$row['position_name'].'</label><br>';
-			}
-			
-			?>
-			
-			
-
-
-
-    <button name = "submit" type="submit" class="contact-submit" data-submit="...Sending...">Add to Database</button>
 		
-		
-		
-	</form>
-	
+		</form>
 	</div>
-	
-	
-	
-
 </body>
-	
-	
-
-
-    
-
-
-
-
 </html>
---
+++ Unit08/navbar.php
@@ -0,0 +1,21 @@
+<!--navigation-->
+	<nav class="navbar navbar-default">
+	<div class = "container-fluid">
+		<div class="navbar-header">
+		
+		<a class="navbar-brand" href="index.php">Unit 08</a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="search.php">Search By Team</a></li>
+			<li><a href="addnewplayer.php">Add Player</a></li>
+			
+		</ul>
+		
+		
+
+		
+		
+		</div>
+	
+	</nav>
